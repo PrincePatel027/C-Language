5bc91f71ce4c2e03bd94d0e53b1b49ff655d0f30
PrincePatel027 <149999800+PrincePatel027@users.noreply.github.com>
2023-11-07 20:23:20 +0530

    Add files via upload

---
+++ DAY 04/fifth.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int x,y;
+
+    clrscr();
+
+    printf("Enter The Number Of X :- ");
+    scanf("%d",&x);
+    printf("Enter The Number Of Y :- ");
+    scanf("%d",&y);
+
+    printf("Your formula answer here :- %d\n",(x+y)^3);
+
+    getch();
+}
---
+++ DAY 04/first.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a,b,c;
+
+    clrscr();
+    
+    printf("Enter the number of A = ");
+    scanf("%d",&a);
+    printf("Enter the number of B = ");
+    scanf("%d",&b);
+
+    c = a ; 
+    a = b ;
+    b = c ;
+
+    printf("Number of A = %d\n",a);
+    printf("Number of B = %d\n",b);
+
+    getch();
+
+}
